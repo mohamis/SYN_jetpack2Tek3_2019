39c43d3ecb2c8e190a17362bfe049337fd8188f6
anthony_lobe <anthony_lobe@localhost.localdomain>
2020-07-16 02:06:53 +0200

    settings manage args okay

--- src/client/manage_args.c
+++ src/client/manage_args.c
@@ -14,6 +14,8 @@ int manage_args(int ac, char **av)
     
     if (ac != 5)
 	    return (84);
-    //if ((strcmp(av[j], "-p")) == 0 && my_isnum(av[j + 1] == 1))
-	  //  return (84);
+    if ((strcmp(av[j], "-p")) != 0 && isdigit(av[j + 1] == 1))
+	    return (84);
+    if ((strcmp(av[i], "-h")) != 0 && av[i + 1] == NULL)
+        return (84);
 }
